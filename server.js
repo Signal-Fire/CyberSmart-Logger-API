43451092ef59d99259718a6c423ae72beb89ae67
Henry Pye <henrypye@outlook.com>
2018-03-30 14:05:49 +0100

    Added Compression module

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 var express = require('express'),
     app = express(),
     cors = require('cors'),
+    compression = require('compression'),
     Add_Routes = require('./Routes/Add'),
     Find_Routes = require('./Routes/Find');
 
 app.use(cors());
+app.use(compression());
 
 app.use('/find', Find_Routes);
 app.use('/create', Add_Routes);
