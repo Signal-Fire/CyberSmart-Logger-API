ba0f571f5bfdb4a190d7af4d55d92eb21e07fddc
Henry <henrypye@outlook.com>
2018-04-15 18:42:37 +0100

    Logger now returns actual JSON objects rather than strings

--- Routes/Add/index.js
+++ Routes/Add/index.js
@@ -2,15 +2,15 @@
 var route = require('express').Router();
 var Creator = new(require('../../Handlers/Add'))();
 
-route.post('/create', function(req, res) {
+route.post('/', function(req, res) {
     try {        
         Creator.CreateLog(req.body).then(log => {
             res.status(201).send(log);
         }).catch(err => {
-            res.status(405).send("{ error : " + err + " }");
+            res.status(405).send({ error : err });
         });
     } catch (ex) {
-        res.status(500).send("{ error : " + ex + " }");
+        res.status(500).send({ex : ex});
     }
 });
 
